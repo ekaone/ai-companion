bc18ae367d96e191d2dbd13066ef138c2c82a542
Eka Prasetia <ekaone3033@gmail.com>
2023-07-22 09:55:19 +0700

    add tones

---
+++ constants/tones.ts
@@ -0,0 +1,13 @@
+export type TTones = {
+  id: number;
+  name: string;
+};
+
+export const tones: TTones[] = [
+  { id: 1, name: "Fun" },
+  { id: 2, name: "Cheerful" },
+  { id: 3, name: "Confident" },
+  { id: 4, name: "Clear" },
+  { id: 5, name: "Conversational" },
+  { id: 6, name: "Convincing" },
+];
