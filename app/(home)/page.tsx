0b91ee94a4311db294a769610de401e13418dbda
Eka Prasetia <ekaone3033@gmail.com>
2023-07-24 18:46:35 +0700

    add page setting

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,39 +1,45 @@
-import Balancer from "react-wrap-balancer";
+import Link from "next/link";
 
 // components
-import CardList from "@/components/home/card-list";
-import { Twitter } from "@/components/shared/icons";
-import { Button } from "@/components/ui/button";
+import Hero from "@/components/layout/hero";
+import { navs } from "@/components/ui/navs";
 
 export default async function Home() {
   return (
     <>
-      <div className="z-10 w-full max-w-2xl px-5 xl:px-0">
-        <h1
-          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
-          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
-        >
-          <Balancer>AI Building blocks for your Next project</Balancer>
-        </h1>
-        <p
-          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
-          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-        >
-          <Balancer>
-            Companion app for your digital assistant and co-worker.
-          </Balancer>
-        </p>
-        <a
-          href="https://twitter.com/twekaone"
-          target="_blank"
-          rel="noreferrer"
-          className="mx-auto mb-5 mt-6 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-          <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Companion
-          </p>
-        </a>
+      <Hero />
+      <div className="z-10 mt-10 space-y-8">
+        {navs.map((section) => {
+          return (
+            <div key={section.name} className="space-y-5">
+              <div className="bg-gradient-to-br from-black to-stone-500 bg-clip-text font-display text-xl font-bold uppercase tracking-wide">
+                {section.name}
+              </div>
+
+              <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+                {section.items.map((item) => {
+                  return (
+                    <Link
+                      href={`/${item.slug}`}
+                      key={item.name}
+                      className="group block space-y-1.5 rounded-lg bg-gray-200 px-5 py-3 hover:bg-gray-400"
+                    >
+                      <div className="font-medium group-hover:text-gray-50">
+                        {item.name}
+                      </div>
+
+                      {item.description ? (
+                        <div className="line-clamp-3 text-sm group-hover:text-gray-50">
+                          {item.description}
+                        </div>
+                      ) : null}
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
---
+++ app/setting/page.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Link from "next/link";
+
+function Page() {
+  return (
+    <div>
+      <div className="grid min-h-screen grid-cols-1 lg:grid-cols-[1fr,1.5fr]">
+        <div className="border p-10">
+          <div className="sticky top-10">
+            <div>
+              <Link
+                href="/dashboard"
+                className="group inline-flex items-center rounded-md border border-gray-300 bg-white py-2 pl-3 pr-4 text-sm font-medium text-gray-700 hover:border-purple-600 hover:text-blue-700 focus:border-purple-200 focus:outline-0 focus:ring-2 focus:ring-purple-300"
+              >
+                ⬅️ Back
+              </Link>
+              <Link
+                href="https://nextjs.org/"
+                target="_blank"
+                rel="noreferrer"
+                className="group inline-flex items-center rounded-md px-2.5 py-1.5 text-sm font-medium tracking-tight text-gray-500 hover:bg-gray-100 hover:text-purple-600 focus:outline-0 focus:ring-2 focus:ring-purple-400"
+              >
+                View Source ↗️
+              </Link>
+            </div>
+            <h2 className="text-4xl font-bold tracking-tight">Template name</h2>
+            <div className="mt-5 space-y-2 text-sm text-gray-600">
+              <p className="flex items-center">📸 Prompt</p>
+              <p className="flex items-center">🗺️ Context</p>
+              <p className="flex items-center">🗓️ Zero-shot</p>
+            </div>
+            <div className="mt-5 space-y-5 text-sm text-gray-500">
+              Lorem impsum
+            </div>
+          </div>
+        </div>
+        <div className="flex overflow-hidden border p-10">
+          Generated contents here
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
